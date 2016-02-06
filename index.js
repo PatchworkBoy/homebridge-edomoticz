9097881ec3559328c705d9d616c121383701233d
PatchworkBoy <marci@MCO-rMBP.local>
2016-02-06 13:14:10 +0000

    fix auth header error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // _Extended_ (e)Domoticz Platform Plugin for HomeBridge by Marci [http://twitter.com/marcisshadow]
-// V0.0.8 - 2016/02/05
+// V0.1.2 - 2016/02/05
 //    - Added SSL Protocol support
 //    - authorization header rather than within URL
 // V0.0.7 - 2016/02/05
@@ -85,8 +85,8 @@ module.exports = function(homebridge) {
   fixInheritance(eDomoticzPlatform.SolRad, Characteristic);
   fixInheritance(eDomoticzPlatform.SolRadDeviceService, Service);
 
-  homebridge.registerAccessory("homebridge-eDomoticz", "eDomoticz", eDomoticzAccessory)
-  homebridge.registerPlatform("homebridge-eDomoticz", "eDomoticz", eDomoticzPlatform);
+  homebridge.registerAccessory("homebridge-edomoticz", "eDomoticz", eDomoticzAccessory)
+  homebridge.registerPlatform("homebridge-edomoticz", "eDomoticz", eDomoticzPlatform);
 }
 
 function eDomoticzPlatform(log, config) {
@@ -95,7 +95,8 @@ function eDomoticzPlatform(log, config) {
   this.server = config["server"];
   if (this.server.indexOf(":") > -1 && this.server.indexOf("@") > -1){
     tmparr = this.server.split("@");
-    this.authstr = btoa(tmparr[0]);
+    var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+    this.authstr = Base64.encode(tmparr[0]);
     this.server = tmparr[1];
   }
   this.ssl = config["ssl"];
@@ -579,7 +580,7 @@ eDomoticzAccessory.prototype = {
 		}.bind(this));
 	},
 	getWindDirection: function(callback) {
-		rvar that = this;
+		var that = this;
 		request.get({
 			url: that.status_url,
       header: {
